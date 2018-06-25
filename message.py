660036b149365cc4bce03346d67a09fc7574cc54
Samuel McHale <sam@samuels-mbp-2.home>
2018-06-25 19:01:06 +0100

    Added vigeneres cipher method and supporting methods to message class

--- message.py
+++ message.py
@@ -5,6 +5,7 @@ class message():
     def __init__(self,text):
         self.text = text.lower()
         self.alphabet = list(string.ascii_lowercase)
+        self.vigenereSquare = []
 
     def caesar(self, shift):
         newString = ''
@@ -23,6 +24,24 @@ class message():
             newString += letter
         return newString
 
+    def createVSquare(self):
+        self.vigenereSquare.append(self.alphabet.copy())
+        alpha = self.alphabet
+        for i in range(len(alpha)-2):
+            alpha.append(alpha[0])
+            alpha = alpha[1:]
+            self.vigenereSquare.append(alpha.copy())
 
+    def vigenere(self, key):
+        newString = ''
+        self.createVSquare()
+        m = math.ceil(len(self.text)/len(key))
+        j = len(key)
+        key = m*key
+        key = key[:-1*(m*j - len(self.text))]
+        for i in range(len(self.text)):
+            letter = self.vigenereSquare[self.alphabet.index(key[i])][self.alphabet.index(self.text[i])]
+            newString += letter
+        return newString
     
 
